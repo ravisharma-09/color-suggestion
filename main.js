77a9825dfb8d961a5194084e3393a262f11c08f5
raviisharma09 <2102508788@svyasa-sas.edu.in>
2026-03-28 22:13:32 +0530

    .

--- main.js
+++ main.js
@@ -8,12 +8,21 @@ function randomColor() {
     return color ;
 }
 
-const box = document.querySelector(".colorbox");
-const text = document.querySelector(".colorcard p")
-
-const color = randomColor() ;
-box.style.background = color ;
-text.innerText = color ;
+const main = document.querySelector(".main") ;
+function generateColors() {
+    main.innerHTML = "" ;
+for (let i = 0 ; i< 20 ; i++){
+    const color = randomColor();
+    main.innerHTML +=  `<div class="colorcard">
+            <div class="colorbox" style="background:${color};"></div>
+            <p>${color}</p>
+            <button class="copy">copy</button>
+            <button class="apply">apply</button>
+        </div>`; 
+}}
+generateColors() ;
+const refresh = document.querySelector(".refresh") ;
+refresh.addEventListener("click", generateColors);
 
 const copybtn = document.querySelector(".copy");
 copybtn.addEventListener("click", () => {
@@ -23,3 +32,17 @@ const applybtn = document.querySelector(".apply");
 applybtn.addEventListener("click", () => {
     document.body.style.background = color ;
 });
+main.addEventListener("click", (e) => {
+    const target = e.target ;
+    const card = target.closest(".colorcard");
+    if (!card) return;
+    const color = card.querySelector("p").innerText ;
+    if (target.classList.contains("copy"))
+        {navigator.clipboard.writeText(color);
+            target.innerText ="Copied!" ;
+            setTimeout(() =>{
+                target.innerText = "copy";
+            },6000
+            );}
+    if(target.classList.contains("apply")){document.body.style.background = color;}
+})
